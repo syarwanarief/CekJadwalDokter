931c08e7002412c1be0856ff4e090f09311a64a2
syarwanarief <ariefsyarwan@gmail.com>
2018-12-12 02:22:17 +0700

    fix activity ubah password

--- app/src/main/java/mobile/project/cekjadwaldokter/Akun/UbahPassword.java
+++ app/src/main/java/mobile/project/cekjadwaldokter/Akun/UbahPassword.java
@@ -17,8 +17,14 @@ import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.EmailAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import mobile.project.cekjadwaldokter.R;
 
@@ -41,57 +47,84 @@ public class UbahPassword extends AppCompatActivity {
         final TextView oldPass = (TextView) findViewById(R.id.sandiLama);
         final TextView newPass = (TextView) findViewById(R.id.sandiBaru);
 
-        emaill = email.getText().toString().trim();
-        oldpass = oldPass.getText().toString().trim();
-        newpass = newPass.getText().toString().trim();
-
         sharedpreferences = getSharedPreferences(MyPREFERENCES, Context.MODE_PRIVATE);
         email.setText(sharedpreferences.getString(Emaill, ""));
         email.setEnabled(false);
-
-        final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-        mdatabase = FirebaseDatabase.getInstance().getReference().child("Users").push();
         //final String emaill = user.getEmail();
-        final AuthCredential credential = EmailAuthProvider
-                .getCredential(emaill, oldpass);
 
         simpan.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String myEmail = sharedpreferences.getString(Emaill, "");
+                oldpass = oldPass.getText().toString().trim();
+                final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
 
-                user.reauthenticate(credential)
-                        .addOnCompleteListener(new OnCompleteListener<Void>() {
-                            @Override
-                            public void onComplete(@NonNull Task<Void> task) {
-                                if (task.isSuccessful()) {
-                                    user.updatePassword(newpass).addOnCompleteListener(new OnCompleteListener<Void>() {
-                                        @Override
-                                        public void onComplete(@NonNull Task<Void> task) {
-                                            if (task.isSuccessful()) {
-                                                mdatabase.child("password").setValue(newPass);
-                                                Toast.makeText(getApplicationContext(), "Kata Sandi Baru Disimpan ", Toast.LENGTH_SHORT).show();
-                                                Intent intent = (new Intent(UbahPassword.this,LoginActivity.class));
-                                                startActivity(intent);
-                                                finish();
-                                            }else if (!task.isSuccessful()){
-                                                Toast.makeText(getApplicationContext(), "Kata Sandi Salah ", Toast.LENGTH_SHORT).show();
-                                                oldPass.setFocusable(true);
-                                            }else {
-                                                Toast.makeText(getApplicationContext(), "Kesalahan Terjadi ", Toast.LENGTH_SHORT).show();
-                                            }
+                AuthCredential credential = EmailAuthProvider.getCredential(myEmail, oldpass);
+                if (user == null) {
+                    Toast.makeText(getApplicationContext(), "Error! Silahkan Login Kembali", Toast.LENGTH_SHORT).show();
+                    Intent intent = (new Intent(UbahPassword.this, LoginActivity.class));
+                    startActivity(intent);
+                    finish();
+                } else {
+                    user.reauthenticate(credential).addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if (task.isSuccessful()) {
+                                newpass = newPass.getText().toString().trim();
+                                user.updatePassword(newpass).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<Void> task) {
+                                        final String UserId = user.getUid();
+                                        if (task.isSuccessful() && oldPass.getText() != null) {
+                                            // save to firebase
+                                            FirebaseDatabase.getInstance().getReference().child("Users").child(UserId)
+                                                    .addListenerForSingleValueEvent(new ValueEventListener() {
+                                                                                        @Override
+                                                                                        public void onDataChange(DataSnapshot dataSnapshot) {
+                                                                                            Map<String, Object> postValues = new HashMap<String, Object>();
+                                                                                            for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                                                                                                postValues.put(snapshot.getKey(), snapshot.getValue());
+                                                                                            }
+                                                                                            postValues.put("password", newpass);
+                                                                                            FirebaseDatabase.getInstance().getReference()
+                                                                                                    .child("Users").child(UserId).updateChildren(postValues);
+                                                                                        }
+
+                                                                                        @Override
+                                                                                        public void onCancelled(DatabaseError databaseError) {
+                                                                                        }
+                                                                                    }
+                                                    );
+                                            // sukses
+                                            Toast.makeText(getApplicationContext(), "Kata Sandi Baru Disimpan ", Toast.LENGTH_SHORT).show();
+                                            Intent intent = (new Intent(UbahPassword.this, LoginActivity.class));
+                                            startActivity(intent);
+                                            SharedPreferences.Editor editor = sharedpreferences.edit();
+                                            editor.remove(Emaill);
+                                            editor.remove(Pass);
+                                            editor.commit();
+                                            finish();
+                                        } else if (!task.isSuccessful()) {
+                                            Toast.makeText(getApplicationContext(), "Failed", Toast.LENGTH_SHORT).show();
+                                            oldPass.setFocusable(true);
+                                        } else {
+                                            Toast.makeText(getApplicationContext(), "Kata Sandi Salah ", Toast.LENGTH_SHORT).show();
                                         }
-                                    });
-                                } else {
-                                    Toast.makeText(getApplicationContext(), "Failed Auth ", Toast.LENGTH_SHORT).show();
-                                }
+                                    }
+                                });//update pass
+                            } else {
+                                Toast.makeText(getApplicationContext(), "Gagal Auth! \n Silahkan Login Kembali", Toast.LENGTH_SHORT).show();
+                                Intent intent = (new Intent(UbahPassword.this, LoginActivity.class));
+                                startActivity(intent);
+                                SharedPreferences.Editor editor = sharedpreferences.edit();
+                                editor.remove(Emaill);
+                                editor.remove(Pass);
+                                editor.commit();
                             }
-                        });
+                        }
+                    });//credential
+                }
             }
-        });
-    }
-
-    public String getPassword(){
-
-        return newpass;
+        });//onclick button
     }
 }
